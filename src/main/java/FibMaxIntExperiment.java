03d17182f8c79b980e6dee3071708da58a595314
William Heng <william@williamheng.com>
2015-10-21 16:43:24 +0100

    Experiment to find out largest term representable by primitive Java integer

--- src/main/java/FibMaxIntExperiment.java
+++ src/main/java/FibMaxIntExperiment.java
@@ -1,5 +1,74 @@
+import redis.clients.jedis.Jedis;
+
+import java.math.BigInteger;
+
 /**
  * Created by William Heng(dev) on 21/10/15.
  */
 public class FibMaxIntExperiment {
+    Jedis redis;
+
+    public FibMaxIntExperiment(Jedis redis) {
+        this.redis = redis;
+    }
+
+    public BigInteger fib(int term) {
+/*        BigInteger[] fibSequence = new BigInteger[term];
+
+        for (int i = 0; i < term; i++) {
+            if (i < 2) {
+                fibSequence[i] = new BigInteger(Integer.toString(i));
+            } else {
+                fibSequence[i] = fibSequence[i-2].add(fibSequence[i-1]);
+            }
+        }
+
+        return fibSequence[term-1];*/
+
+        BigInteger f0 = BigInteger.ZERO;
+        BigInteger f1 = BigInteger.ONE;
+        BigInteger fn = null;
+
+        for (int i = 0; i <= term; i++) {
+            String key = Integer.toString(i);
+            if (this.redis.exists(key)) {
+                fn = new BigInteger(this.redis.get(key));
+                f0 = f1;
+                f1 = fn;
+            } else {
+                fn = f0.add(f1);
+                f0 = f1;
+                f1 = fn;
+
+                String fnString = fn.toString();
+                System.out.println("Setting " + key + " as " + fnString);
+                this.redis.set(key, fnString);
+            }
+        }
+
+        return fn;
+    }
+
+    private final static BigInteger INT_MAX_VALUE = new BigInteger(Integer.toString(Integer.MAX_VALUE));
+    public static void main(String[] args) {
+        Jedis redis = new Jedis("localhost");
+        FibMaxIntExperiment experiment = new FibMaxIntExperiment(redis);
+
+        if (!redis.exists("0") || !redis.exists("1") ||
+            !redis.get("0").equals("0") || !redis.get("1").equals("1")
+            ) {
+            redis.flushAll();  // Clear everything and set default values
+            redis.set("0", "0");
+            redis.set("1", "1");
+        }
+
+        for (int term = 0; term < Integer.MAX_VALUE; term++) {
+            if (experiment.fib(term).compareTo(INT_MAX_VALUE) > 0) {
+                // Found the upper limit
+                int representableTerm = term-1;
+                System.out.println("The highest term a primitive integer can represent is term " + representableTerm);
+                break;
+            }
+        }
+    }
 }
